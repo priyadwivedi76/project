37f6752b9baf6abf3aa243e2fa4035e3e64864c8
Dwivedi Priya <pd7668665@gmail.com>
2024-04-13 17:13:33 +0530

    basic model

--- models/listing.js
+++ models/listing.js
@@ -8,9 +8,10 @@ const listingSchema=new Schema({
     description:String,
     image:{
         type:String,
+        default:"https://images.unsplash.comc/photo-1712939707022-c9aba658184f?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
         set:(v)=>
         v===""?
-        "https://images.unsplash.com/photo-1712939707022-c9aba658184f?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+        "https://images.unsplash.comc/photo-1712939707022-c9aba658184f?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
         :v,
     },
     price:Number,
