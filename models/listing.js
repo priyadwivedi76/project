6a35e8049d837f0c5975793d5cc0e038fcbf2964
Dwivedi Priya <pd7668665@gmail.com>
2024-04-13 16:35:33 +0530

    schema

--- models/listing.js
+++ models/listing.js
@@ -1,13 +1,22 @@
 const mongoose=require("mongoose");
-
+const Schema=mongoose.Schema; 
 const listingSchema=new Schema({
-    title:String,
+    title:{
+        type:String,
+        required:true,
+    },
     description:String,
-    image:String,
+    image:{
+        type:String,
+        set:(v)=>
+        v===""?
+        "https://images.unsplash.com/photo-1712939707022-c9aba658184f?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+        :v,
+    },
     price:Number,
     location:String,
     country:String
 });
 
 const Listing=mongoose.model("Listing",listingSchema);
-module.export=Listing;
+module.exports=Listing;
