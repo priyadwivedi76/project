37ac0ac983d767e100fbc5420f6a1ff71d04f350
Dwivedi Priya <pd7668665@gmail.com>
2024-04-13 16:35:13 +0530

    basic model

---
+++ index.js
@@ -0,0 +1,44 @@
+const express=require("express");
+const app=express();
+const mongoose=require("mongoose");
+const Listing=require("./models/listing.js");
+
+const mongoose_url="mongodb://127.0.0.1:27017/wanderlust";
+
+main()
+.then(()=>{
+    console.log("Connection to dbs is secured");
+})
+.catch(err=>{
+    console.log(err);
+})
+
+async function main() {
+    await mongoose.connect(mongoose_url);
+}
+
+
+app.get("/",(req,res)=>{
+    res.send("Welcome to the root");
+});
+
+app.get("/testListing",async(req,res)=>{
+    let sampleListing= new Listing({
+        title:"My Villa",
+        description:"New,Affordable and Semi Furnished",
+        price:12000,
+        location:"Goa",
+        country:"India"
+    });
+
+    await sampleListing.save();
+    console.log("Sample was saved");
+    res.send("Successful");
+})
+app.listen(8080,()=>{
+    console.log("Listening to the port 8080");
+})
+
+
+
+
