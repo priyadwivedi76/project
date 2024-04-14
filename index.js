88e2634ef9440e2b42a1740e5a60ccbcc1bb7bb6
Dwivedi Priya <pd7668665@gmail.com>
2024-04-14 19:15:32 +0530

    working with db

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express=require("express");
 const app=express();
 const mongoose=require("mongoose");
 const Listing=require("./models/listing.js");
-
+const path=require("path");
 const mongoose_url="mongodb://127.0.0.1:27017/wanderlust";
 
 main()
@@ -22,19 +22,26 @@ app.get("/",(req,res)=>{
     res.send("Welcome to the root");
 });
 
-app.get("/testListing",async(req,res)=>{
-    let sampleListing= new Listing({
-        title:"My Villa",
-        description:"New,Affordable and Semi Furnished",
-        price:12000,
-        location:"Goa",
-        country:"India"
+app.get("/listing",async(req,res)=>{
+     const allListing=await Listing.find({}).then((res)=>{
+        res.render("index.ejs",{allListing});
     });
+});
+// app.get("/testListing",async(req,res)=>{
+//     let sampleListing= new Listing({
+//         title:"My Villa",
+//         description:"New,Affordable and Semi Furnished",
+//         price:12000,
+//         location:"Goa",
+//         country:"India"
+//     });
 
-    await sampleListing.save();
-    console.log("Sample was saved");
-    res.send("Successful");
-})
+//     await sampleListing.save();
+//     console.log("Sample was saved");
+//     res.send("Successful");
+// })
+
+
 app.listen(8080,()=>{
     console.log("Listening to the port 8080");
 })
