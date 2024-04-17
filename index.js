9607e7b12e35e13e5520f1386e8a8f19f5fbcc47
Dwivedi Priya <pd7668665@gmail.com>
2024-04-17 10:44:21 +0530

    wanderlust

--- index.js
+++ index.js
@@ -27,6 +27,7 @@ app.set("view engine","ejs");
 app.set("views",path.join(__dirname,"views"));
 app.use(methodOverride("_method"));
 app.engine("ejs",ejsMate);
+app.use(express.static(path.join(__dirname,"/public")));
 
 app.get("/",(req,res)=>{
     res.send("Welcome to the root");
